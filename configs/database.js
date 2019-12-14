7a42604d80d2694ebc047d3c4b34e3af11065c9e
Ori <ori.volfo@gmail.com>
2019-12-14 09:12:30 +0200

    move DB init to database module

--- configs/database.js
+++ configs/database.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const axios = require('axios');
 
 mongoose.connect('mongodb://localhost:27017/UsersDB');
 
@@ -7,3 +8,19 @@ const db = mongoose.connection;
 db.once('open', () => console.log('Database opened...'));
 db.on('error', () => console.log('Error occured..'));
 
+let DB = {};
+
+DB.init = function(usersInitURL) {
+    usersInit(usersInitURL);
+};
+
+async function usersInit(url){
+
+    let users = await axios.get(url);
+    users.data.forEach(user =>{
+            axios.post('http://localhost:8000/api/users/',user);
+        }
+    )
+}
+
+module.exports = DB;
--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
 const args = require('minimist')(process.argv.slice(2));
-const axios = require('axios');
 const usersInitURL = 'https://jsonplaceholder.typicode.com/users';
 const bodyParser = require('body-parser');
-
-require('./configs/database');
+const  DB = require('./configs/database');
 
 app.use(bodyParser.urlencoded({extended : true}))
     .use(bodyParser.json());
@@ -17,16 +15,5 @@ app.listen(8000);
 
 
 if(args.initDB){
-    initDB();
-}
-function initDB() {
-    usersInit(usersInitURL);
-}
-
-async function usersInit(url){
-    let users = await axios.get(url);
-    users.data.forEach(user =>{
-        axios.post('http://localhost:8000/api/users/',user);
-        }
-    )
+    DB.init(usersInitURL);
 }
