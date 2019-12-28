5b66dcebf9215238c69c69459c1fa59dbd2377f3
Ori <ori.volfo@gmail.com>
2019-12-28 12:58:49 +0200

    Posts API part 2

--- routes/postsRoute.js
+++ routes/postsRoute.js
@@ -16,17 +16,17 @@ router.route('/')
         })
     });
 
-// // Find post by ID
-// router.route('/:id')
-//     .get(function(req,resp){
-//         Post.find( {id:req.params.id} , function(err,per){
-//             if(err){
-//                 return resp.send(err);
-//             }
-//             return resp.json(per);
-//         })
-//     });
-//
+// Find post by ID
+router.route('/:id')
+    .get(function(req,resp){
+        Post.find( {id:req.params.id} , function(err,per){
+            if(err){
+                return resp.send(err);
+            }
+            return resp.json(per);
+        })
+    });
+
 // Insert new post
 router.route('/')
     .post(function(req,resp){
@@ -47,38 +47,38 @@ router.route('/')
         })
     });
 
-// // Update post by ID
-// router.route('/:id')
-//     .put(async function(req,resp){
-//
-//         const response = await axios.get('http://localhost:8000/api/posts/'+req.params.id);
-//         let updatedPost = response.data;
-//
-//         for(let key in req.body){
-//             updatedPost[key] = req.body[key];
-//         }
-//         Post.findOneAndUpdate({id:req.params.id},updatedPost,function(err){
-//             if(err){
-//                 return resp.send(err);
-//             }
-//             else{
-//                 return resp.send(`Post ${req.body.username} was updated!`);
-//             }
-//         })
-//     });
-//
-// // Delete post by ID
-// router.route('/:id')
-//     .delete(function(req,resp){
-//
-//         Post.findByIdAndDelete(req.params.id,function(err){
-//             if(err){
-//                 return resp.send(err);
-//             }
-//             else{
-//                 return resp.send(`Post ${req.body.username} was deleted!`);
-//             }
-//         })
-//     });
+// Update post by ID
+router.route('/:id')
+    .put(async function(req,resp){
+
+        const response = await axios.get('http://localhost:8000/api/posts/'+req.params.id);
+        let updatedPost = response.data;
+
+        for(let key in req.body){
+            updatedPost[key] = req.body[key];
+        }
+        Post.updateOne({id:req.params.id},updatedPost,function(err){
+            if(err){
+                return resp.send(err);
+            }
+            else{
+                return resp.send(`Post ${req.params.id} was updated!`);
+            }
+        })
+    });
+
+// Delete post by ID
+router.route('/:id')
+    .delete(function(req,resp){
+
+        Post.findOneAndDelete({id:req.params.id},function(err){
+            if(err){
+                return resp.send(err);
+            }
+            else{
+                return resp.send(`Post ${req.params.id} was deleted!`);
+            }
+        })
+    });
 
 module.exports = router;
